cd9afcde9c298ffb534e34d3e4c22c028df0e449
Zac Heisey <zheisey@gmail.com>
2019-07-25 10:13:05 -0400

    create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Write your JavaScript below these comments...
+/*
+  Hint #1: Which element in the DOM do we need to "grab"
+  and listen for changes to?
+
+  Hint #2: How do we access the VALUE that is INPUT into
+  the element from Hint #1?
+*/
